d88f5b14e8ddd5cb826f3003df9642c0bded15d8
Aleks Vino <aleks.vino@gmail.com>
2018-01-19 22:44:19 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,5 @@
 var playlist = {Nsync:"Bye-Bye-Bye", Backstreet:"Everybody", Brittney:"Lucky"};
 
-playlist.Christina = 'Dirty';
-
 function updatePlaylist(playlist, 'Christina', 'Dirrrty') {
   playlist[Christina] = 'Dirrrty';
   
